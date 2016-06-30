06e1b7517225d102a816eb0afd17ecdf8aa22cca
Orry Baram <orrybaram@gmail.com>
2016-06-30 16:19:52 -0400

    updated counter to remove unnesesarcy functions

--- client/components/Counter.jsx
+++ client/components/Counter.jsx
@@ -2,21 +2,16 @@ import element from 'magic-virtual-element';
 import {observer} from 'mobx-deku';
 
 export default observer({
-  render ({props}) {
+  render ({props: {state}}) {
     return (
       <div class='counter'>
-        <div class='counter__display'>{props.state.counter}</div>
+        <div class='counter__display'>{state.timer}</div>
+        <div class='counter__display'>{state.counter}</div>
         <div>
-          <button class='button' onClick={decreaseCounter}>-</button>
-          <button class='button' onClick={increaseCounter}>+</button>
+          <button class='button' onClick={state.decreaseCounter.bind(state)}>-</button>
+          <button class='button' onClick={state.increaseCounter.bind(state)}>+</button>
         </div>
       </div>
     );
-    function increaseCounter () {
-      props.state.increaseCounter();
-    }
-    function decreaseCounter () {
-      props.state.decreaseCounter();
-    }
   }
 });
